abc744dd8492475c8c42451e36a29a7106e1984b
Shreyans Jain <shreyansj551@gmail.com>
2019-06-15 18:50:02 +0530

    use Link to prevent page reload on every click event

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 // import axios from "axios";
-import {Route} from 'react-router-dom'
+import { Route, Link } from "react-router-dom";
 
 import classes from "./Blog.css";
-import Posts from './Posts/Posts';
-import NewPost from '../NewPost/NewPost'
+import Posts from "./Posts/Posts";
+import NewPost from "../NewPost/NewPost";
 
 class Blog extends Component {
   render() {
@@ -13,13 +13,17 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New post</a></li>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/new-post">New post</Link>
+              </li>
             </ul>
           </nav>
         </header>
-        <Route path="/" exact component={Posts} /> 
-        <Route path="/new-post" component={NewPost} /> 
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
